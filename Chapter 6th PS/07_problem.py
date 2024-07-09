8bf3283c654aa6a7d74064522def95ea353ae899
CodeWithPerry <codewithperry@gmail.com>
2024-07-09 15:22:45 +0000

    Detailed a line.

--- Chapter 6th PS/07_problem.py
+++ Chapter 6th PS/07_problem.py
@@ -2,7 +2,7 @@ post = "Perry is a brilliant programmer.\nHe everyday tries to learn something n
 
 
 if (
-    "Perry".lower() in post.lower()
+    "Perry".lower() in post.lower() #converting both perry to lower case and the entire post also
 ):  # Lower is used if Perry is written as perry it will still detect as it will convert Perry to all lower and detect with the lowner case powt.
 
     print("This post is about Perry")
